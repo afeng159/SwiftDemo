8a5d62835db485b2828624eea6b0a7ddf7f99e00
424425549@qq.com <424425549@qq.com>
2017-09-08 20:22:47 +0800

    任务：Demo1
    时间：
    姓名：陈旭锋

--- Demo1/Demo1/ViewController.swift
+++ Demo1/Demo1/ViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+
+    
+    var Counter = 0.0
+    var Timer = Foundation.Timer()
+    var IsPlaying = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.label.text = String(Counter);
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func playBtnClick(_ sender: UIButton) {
+        
+        if(IsPlaying) {
+            return
+        }
+      
+        Timer = Foundation.Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.UpdateTimer), userInfo: nil, repeats: true)
+        IsPlaying = true
+
+        
+        
+        
+    }
+    func UpdateTimer() {
+        Counter = Counter + 0.1
+        self.label.text = String(format: "%.1f", Counter)
+    }
+
+    @IBAction func pauseBtnClick(_ sender: Any) {
+        
+        if(!IsPlaying) {
+            return
+        }
+        
+        Timer.invalidate();
+        
+        IsPlaying = false;
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
